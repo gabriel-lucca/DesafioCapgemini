bb26d4d84d452cc7b2f2d12de2400ab986ba8093
Gabriel <Gabriel@DESKTOP-BR5EK8R>
2022-02-17 19:28:28 -0300

    questao 1

---
+++ src/main/java/Questao01/piramide.java
@@ -0,0 +1,26 @@
+package Questao01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class piramide {
+
+	public List<String> cTamanho(int tamanho) {
+		List<String> linhas = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+
+		for (int linha = 0; linha < tamanho; linha++) {
+			for (int coluna = tamanho - 1; coluna >= 0; coluna--) {
+
+				if (coluna <= linha) {
+					sb.append('*');
+				} else {
+					sb.append(' ');
+				}
+			}
+			linhas.add(sb.toString());
+			sb = new StringBuilder();
+		}
+		return linhas;
+	}
+}
---
+++ src/main/java/Questao01/questao01.java
@@ -0,0 +1,27 @@
+package Questao01;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class questao01 {
+
+	private static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * A base e altura da escada devem ser iguais ao valor de n. A última linha não
+	 * deve conter nenhum espaço
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.print("Digite um número até 10: ");
+		int tamanho = sc.nextInt();
+		sc.close();
+
+		List<String> linhas = new piramide().cTamanho(tamanho);
+
+		linhas.forEach(System.out::println);
+
+	}
+}
